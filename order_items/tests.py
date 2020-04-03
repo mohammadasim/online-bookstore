42114d5cfdaa981d9ed1b6c8dbc0636fbe22acad
mohammad.ayub <asimayub81@gmail.com>
2020-04-03 08:57:53 +0100

    added more test to order_items

--- order_items/tests.py
+++ order_items/tests.py
@@ -1,12 +1,10 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.urls import resolve
+from django.test import TestCase, Client
 
 from books.models import Book
 from orders.models import CustomerOrder
 from payments.models import CustomerPayment
 from .models import OrderItem
-from .views import order_item_create_view
 
 
 class OrderItemTests(TestCase):
@@ -21,7 +19,8 @@ class OrderItemTests(TestCase):
         self.book = Book.objects.create(
             title='Deep work',
             author='Cal Newport',
-            price=50.00
+            price=50.00,
+            quantity=10
         )
         self.payment_id = CustomerPayment.objects.create(
             customer_id=self.user
@@ -36,6 +35,8 @@ class OrderItemTests(TestCase):
             quantity=3,
             order_id=self.order
         )
+        self.client = Client()
+        self.client.force_login(self.user)
 
     def test_created(self):
         self.assertEqual(self.order_item.book, self.book)
@@ -43,26 +44,46 @@ class OrderItemTests(TestCase):
         self.assertEqual(self.order_item.quantity, 3)
         self.assertEqual(self.order_item.price, self.book.price * self.order_item.quantity)
 
-    def test_order_item_create_view(self):
+    def test_order_item_create_view_get(self):
         """
         Test logged in user can create an order_item
         """
-        self.client.force_login(self.user)
         response = self.client.get(self.book.get_add_to_cart_url(), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'order_items/order_item_create.html')
         self.assertContains(response, self.order_item.book.title)
         self.assertContains(response, 'Order Book')
         self.assertNotContains(response, 'Hello World')
-        response = self.client.post(self.book.get_add_to_cart_url(), {
-            'book': self.book,
-            'quantity': 1
+
+    def test_order_item_create_view_post(self):
+        new_book = Book.objects.create(
+            title='Harry Potter',
+            author='JK Rowling',
+            price=50.00,
+            quantity=10
+        )
+        response = self.client.post(new_book.get_add_to_cart_url(), {
+            'book': new_book,
+            'quantity': 1,
+            'order_id': self.order
         }, follow=True)
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(OrderItem.objects.all().count(), 2)
+        order_item = OrderItem.objects.get(book=new_book)
+        self.assertEqual(order_item.book, new_book)
+        self.assertEqual(order_item.quantity, 1)
 
-    def test_order_item_create_item_order_resolves_order_item_create_view(self):
-        view = resolve(self.book.get_add_to_cart_url())
-        self.assertEqual(
-            view.func.__name__,
-            order_item_create_view.as_view().__name__
-        )
+    def test_order_item_update_get(self):
+        response = self.client.get(self.order_item.get_update_url(), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'order_items/order_item_update.html')
+
+    def test_order_item_update_post_increment(self):
+        response = self.client.post(self.order_item.get_update_url(), {
+            'book': self.book,
+            'quantity': 5,
+            'order_id': self.order
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        order_item = OrderItem.objects.get(book=self.book)
+        self.assertEqual(order_item.quantity, 5)
